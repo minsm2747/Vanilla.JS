66dfc8800914c38d5c326d27dba6d7b8cee79320
minsm0618 <su960618@gmail.com>
2020-10-22 20:47:32 +0900

    바닐라 JS로 웹 그림판 제작 Ver 04

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("controls__color jsColor");
 
 canvas.width = 700;
 canvas.height = 700;
@@ -26,12 +27,12 @@ function onMouseMove(event) {
     } else {
         ctx.lineTo(x, y);
         ctx.stroke();
-        ctx.closePath();
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if (canvas) {
@@ -40,3 +41,5 @@ if (canvas) {
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
